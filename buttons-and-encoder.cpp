116ac8aec2b668d4a5deae9c5f7baffb04db1b3b
Nikita A Menkovich <menkovich@gmail.com>
2010-11-09 22:28:29 +0300

    initial commit

---
+++ buttons-and-encoder.cpp
@@ -0,0 +1,129 @@
+int ENCA_cur, ENCA_prev; // pin A encoder state
+int ENCB_cur, ENCB_prev; // pin B encoder state
+int BTN_cur, BTN_prev; // pin btn status
+void main (void)
+{
+	SCS |= 0x01;
+	FIO0DIR = 0x00000000; 
+	FIO2DIR = 0x00FF; 
+	FIO2MASK = 0; 
+	while (1)//Loop forever
+	{
+		int a;
+		int freeze;
+		int status;
+		int top, left, right, bottom;
+		int memory; // variable that set from which step we will begin scale;
+		int speed = 3;
+		int delay;
+		left = GetBTNState(1);
+		top	= GetBTNState(2);
+		if(!memory) memory = 1; //in the beggining memory os not set
+		if (left) // btn 1 pressed
+		{
+			a = memory; // set step
+			while (a <= 9) // infinite loop, really
+			{
+				status = GetEncState(); // take status of encoder
+				top	= GetBTNState(2); // stop or not?
+				if (status == 1) speed++; // speed increase
+				if (status == -1) speed--; // speed decrease
+				delay = power(10,speed); // set delay between taking status, turning on lights, etc
+				// BUG:
+				// It is better to realise getting status on interrupts, because 
+				// we will get it only when delay ended
+				FIO2PIN = Scale(a); 
+				Delay(delay);
+				if (top) // btn 2 pressed
+				{
+					memory = a; // remember step we are stopped
+					break; // get out of loop
+				}
+				a++;
+				if (a == 8) a = 1; // if we on the end of scale, we will move to begin of it
+			}
+		}
+		Delay(1); // immediate start if button pressed
+	}
+}
+int GetEncState()
+{
+	int state = 0;
+
+	ENCA_cur = (FIO0PIN & 0x00000800) >> 11; //Чтение текущего состояния контакта А энкодера (P0.11)
+	ENCB_cur = (FIO0PIN & 0x00200000) >> 21; //Чтение текущего состояния контакта B энкодера (P0.21)
+
+	if( (ENCA_cur != ENCA_prev) )	//Если состояние контакта А энкодера изменилось
+	{
+		if( !ENCA_cur )		//Если текущее состояние контакта А энкодера - 0 (переход из 1)
+		{
+			if( !ENCB_cur )	//Если текущее контакта B энкодера - 0
+				state = 1;	//Переменная состояния означает, что вращение по часовой
+			else
+				state = -1;	//Переменная состояния означает, что вращение против часовой
+		}
+		else				//Если текущее состояние контакта А энкодера - 1 (переход из 0)
+		{
+			if( ENCB_cur )	//Если текущее контакта B энкодера - 1
+				state = 1;	//Переменная состояния означает, что вращение по часовой
+			else
+				state = -1;	//Переменная состояния означает, что вращение против часовой
+		}
+	}
+
+	ENCA_prev = ENCA_cur;	//Текущее состояние сохраняется в переменных,
+							//которые будут предыдущими в следующем цикле проверки
+	ENCB_prev = ENCB_cur;
+
+	return state;	//Возвращаем состояние 0 если энкодер не вращается, 
+					//1 если вращается по часовой и -1 если против часовой
+}
+
+int getBTNState(int btn)
+{
+	int state = 0;
+	switch (btn) 
+	{
+		case 1:
+			BTN_cur = FIO0PIN & 0x0020; // button 1
+		case 2:
+			BTN_cur = FIO0PIN & 0x0040; // button 2
+		case 3:
+			BTN_cur = FIO0PIN & 0x0080; // button 3
+		case 4:
+			BTN_cur = FIO0PIN & 0x0200; // button 4
+		default: 
+			BTN_cur = FIO0PIN & 0x0020; // default button 1
+	}
+	if( BTN_cur != BTN_prev) // state changed
+	{
+		if( !BTN_cur ) // if zero, button pressed
+			state = 1; 
+	}
+	BTN_prev = BTN_cur;	 // remember current button state
+	return state; // 0 if do not pressed, 1 if pressed
+}
+
+
+int Scale(int counter)
+{
+	int a;
+	int result = 0x00;
+	int result_prev = 0x03;
+	for (a=1; a<=counter; a++)
+	{
+		result = result+result_prev;
+		result_prev= result;
+	}
+	return result;
+}
+
+double power(double x, long n) //rescursive power() function from wiki
+{
+	double tmp; 
+	if(n == 0) return 1;
+	if(n < 0) return power ( 1 / x, -n);
+	if(n % 2) return x * power (x, n - 1);
+	return power(x * x, n / 2);
+ }
+
---
+++ lab1(vars-7-8-9).c
@@ -0,0 +1,99 @@
+static void Delay(DWORD tick)
+{
+	while(tick--)
+	{
+		//Do Nothing
+	}
+}
+
+void main (void)
+{
+	// Настроить порт
+	SCS |= 0x01; //Разрешить быстрый ввод/вывод
+	FIO0DIR = 0x00000000; // Все разряды порта 0 на ввод для чтения состояния кнопок и энкодера
+	FIO0MASK = 0; //Все разряды порта 0 работают в быстром режиме
+	FIO2DIR = 0x00FF; // Биты 0-7 порта 2 на вывод для управления светодиодами
+	FIO2MASK = 0; //Все разряды порта 2 работают в быстром режиме
+	int task;
+	int a;
+	int delay;
+	task = 8;
+	while (1)//Loop forever
+	{
+		if (task == 7)
+		{
+			int memory;
+			int dec;
+			FIO2PIN = 0x00; // Turn off lights
+			delay = 100000; // Base delay
+			for (a=1; a<=8; a++) // oooo -> ooo0 -> oo00 -> o000 -> 0000
+			{
+				Delay( delay );
+				FIO2PIN = FIO2PIN*0x02+0x01; // level^2+1
+			}
+			Delay( delay*3 );
+			memory = 0xFF;
+			dec=0x100;
+			for (a=8; a>=1; a--) // 0000 -> o000 -> oo00 -> ooo0 -> oooo
+			{
+				Delay( delay );
+				dec = dec/0x02;
+				memory = memory - dec;
+				FIO2PIN = memory;
+			}
+			Delay( delay );
+			FIO2PIN = 0x00;
+			
+		} else if (task == 8)
+		{
+			int memory;
+			int dec;
+			FIO2PIN = 0x00;
+			delay = 100000;
+			for (a=1; a<=8; a++) // oooo -> ooo0 -> oo00 -> o000 -> 0000
+			{
+				Delay( delay );
+				FIO2PIN = FIO2PIN*0x02+0x01;
+			}
+			Delay( delay*3 );
+			memory = 0xFF;
+			dec=0x00;
+			for (a=8; a>=1; a--) // 0000 -> o000 -> oo00 -> ooo0 -> oooo
+			{
+				Delay( delay );
+				dec = dec*0x02+0x01;
+				FIO2PIN = memory-dec;
+			}
+			Delay( delay );
+			FIO2PIN = 0x00;
+		} else if (task == 9)
+		{ 
+			int memory;
+			int inc;
+			int i;
+			int max = 8;
+			FIO2PIN = 0x00;
+			delay = 100000;
+			Delay(delay);
+			memory = 0x00;
+			for (i=1; i<=8; i++) 
+			// oooo -> ooo0 -> oo0o -> o0oo -> 0ooo -> 0oo0 ->
+			// -> 0o0o -> 00oo -> 00o0 -> 000o -> 0000
+			{
+				inc = 0x01;
+				// we will descrease max value, because we
+				// need to add lamps in cycle to an existed one's
+				for (a=1; a<=max; a++)
+				{
+					Delay( delay );
+					FIO2PIN = memory+inc;
+					inc=inc*0x02;
+				}
+				memory = FIO2PIN; // here we remember current lamp state after cycle
+				max--; // decrease total lamps to cycle
+				Delay(delay*2);
+			}
+		}
+		Delay(delay*20);
+	}
+}
